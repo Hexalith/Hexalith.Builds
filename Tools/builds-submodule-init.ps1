359b564af51beb3672f3de8942f511591108493c
Jérôme Piquot <jpiquot@itaneo.com>
2025-03-23 07:46:33 +0100

    Enhance builds-submodule-init.ps1 for submodule management
    - Added initialization and update commands for the Hexalith.Builds Git submodule.
    - Included a command to pull the latest changes for all submodules.
    - Created a symbolic link for the .editorconfig file from the Hexalith.Builds submodule, ensuring consistent configuration across projects.

--- Tools/builds-submodule-init.ps1
+++ Tools/builds-submodule-init.ps1
@@ -1,4 +1,12 @@
+# Initialize the Hexalith.Builds Git submodule from the specified repository
 git submodule init https://github.com/Hexalith/Hexalith.Builds.git
+
+# Update the Hexalith.Builds submodule to the latest commit referenced in the parent repo
 git submodule update Hexalith.Builds
+
+# Execute 'git pull origin main' for each submodule to ensure they're all up to date
 git submodule foreach git pull origin main
+
+# Create a symbolic link for the .editorconfig file pointing to the one in the Hexalith.Builds submodule
+# -Force overwrites the link if it already exists
 New-Item -ItemType SymbolicLink -Path ".editorconfig" -Target "./Hexalith.Builds/.editorconfig" -Force
