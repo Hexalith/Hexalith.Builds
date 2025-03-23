98d09cd88f6a4a192e34d7a1b9beb4fba916bc3e
Jérôme Piquot <jpiquot@itaneo.com>
2025-03-23 10:09:25 +0100

    Refactor project configuration and update tests
    - Removed C# file header template from `.editorconfig`, now defined in `Hexalith.globalconfig`.
    - Updated `Hexalith.Builds` project with new commit reference and added files to `Hexalith.Commons.sln`.
    - Refactored `StringHelperTest.cs` to remove copyright block and use expression-bodied members.
    - Added `StyleCop.Analyzers` package reference in `Hexalith.Build.props` for improved code style enforcement.
    - Included reference to `Hexalith.globalconfig` in `Hexalith.Package.props` for global analyzer settings.
    - Modified `builds-submodule-init.ps1` to remove symbolic link creation for `.editorconfig`.
    - Introduced `Hexalith.globalconfig` for consistent licensing information across C# files.

--- Tools/builds-submodule-init.ps1
+++ Tools/builds-submodule-init.ps1
@@ -6,7 +6,3 @@ git submodule update Hexalith.Builds
 
 # Checkout the main branch in the Hexalith.Builds submodule
 git submodule foreach git checkout main
-
-# Create a symbolic link for the .editorconfig file pointing to the one in the Hexalith.Builds submodule
-# -Force overwrites the link if it already exists
-New-Item -ItemType SymbolicLink -Path ".editorconfig" -Target "./Hexalith.Builds/.editorconfig" -Force
