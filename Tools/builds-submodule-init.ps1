3899f4cdb67543e92b07b57e0534d0833dc0fb45
Jérôme Piquot <jpiquot@itaneo.com>
2025-03-23 07:41:17 +0100

    Update symbolic link creation in builds-submodule-init.ps1
    - Replaced the `ln -s` command with `New-Item` for creating a symbolic link to `.editorconfig`, ensuring compatibility with PowerShell scripting.

--- Tools/builds-submodule-init.ps1
+++ Tools/builds-submodule-init.ps1
@@ -1,4 +1,4 @@
 git submodule init https://github.com/Hexalith/Hexalith.Builds.git
 git submodule update Hexalith.Builds
 git submodule foreach git pull origin main
-ln -s ./Hexalith.Builds/.editorconfig .editorconfig
+New-Item -ItemType SymbolicLink -Path ".editorconfig" -Target "./Hexalith.Builds/.editorconfig" -Force
