b24394da0d910fa320efcc45cdbe0f451ef0204f
Jérôme Piquot <jpiquot@itaneo.com>
2025-03-23 07:48:21 +0100

    Refine builds-submodule-init.ps1 for improved submodule management
    - Changed the command to checkout the main branch in the Hexalith.Builds submodule instead of pulling the latest changes for all submodules.
    - This adjustment ensures that the submodule is on the correct branch for consistent development.

--- Tools/builds-submodule-init.ps1
+++ Tools/builds-submodule-init.ps1
@@ -4,8 +4,8 @@ git submodule init https://github.com/Hexalith/Hexalith.Builds.git
 # Update the Hexalith.Builds submodule to the latest commit referenced in the parent repo
 git submodule update Hexalith.Builds
 
-# Execute 'git pull origin main' for each submodule to ensure they're all up to date
-git submodule foreach git pull origin main
+# Checkout the main branch in the Hexalith.Builds submodule
+git submodule foreach git checkout main
 
 # Create a symbolic link for the .editorconfig file pointing to the one in the Hexalith.Builds submodule
 # -Force overwrites the link if it already exists
